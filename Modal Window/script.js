7bd6c5349f9c5aae6a41f4c88636081f8af11ac9
Francisco Ramos <110337302+Frara46@users.noreply.github.com>
2023-03-30 22:59:42 -0400

    Add files via upload

---
+++ Modal Window/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+//OPENING the modal window
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+//CLOSING the modal window
+//adding and removing classes can activate and deactivate sertan styles
+// at the same time
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//Ecs key Press Event
